c10ba1b30b67421f600fae7317e559f5a36a01e5
SolonikovaAlbina <99264411+SolonikovaAlbina@users.noreply.github.com>
2022-04-05 23:25:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,7 +5,7 @@
 
 template<typename T>
 class TPQueue {
-  private:
+ private:
   T *arr;
   int size;
   int begin, end;
@@ -22,7 +22,7 @@ class TPQueue {
       res -= size + 1;
     return res;
   }
-  public:
+ public:
   TPQueue() :
   size(100),
   begin(0), end(0), count(0) {
